65e7b8fbe50a2ac9917e8d0016cec658067915f5
ElizavetaKagan <76471759+ElizavetaKagan@users.noreply.github.com>
2023-12-17 23:44:35 +0300

    Delete gvm_view.sql

--- gvm_view.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE OR REPLACE VIEW public.newview
-AS SELECT p.category_id,
-    p2.store_id,
-    sum(pi2.product_count::numeric * pi2.pruduct_price) AS sales_sum
-   FROM purchase_items pi2
-     JOIN products p ON pi2.product_id = p.product_id
-     JOIN purchases p2 ON pi2.purchase_id = p2.purchase_id
-  GROUP BY p.category_id, p2.store_id;
